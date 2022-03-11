f89c3eb83415b388b32f794e920cd4b5d23756be
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-11 02:21:19 -0800

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-gcc man.c
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: value 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
