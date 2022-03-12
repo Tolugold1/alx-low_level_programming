78753af334b7ddd4c38999e543fe423e74d7c539
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-12 01:04:32 -0800

    Task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: absolute 0 (success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
