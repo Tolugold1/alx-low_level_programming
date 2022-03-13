aef0865a9cef02db40e369ed2c830fab9164f674
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-13 08:04:00 -0700

    Task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: absolute 0 (seccess)
+ */
+
+int main(void)
+{
+	int i;
+
+	do {
+		for (i = 0; i < 10; i++)
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	} while (i < 10);
+	return (0);
+}
