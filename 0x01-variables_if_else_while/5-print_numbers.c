631e18c1563d9fbc4a6237a897b4adcb1bacaa85
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-13 06:01:48 -0700

    Task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all digit less than 10
+ *
+ * Return: absolute 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d",n);
+	}
+	printf("\n");
+	return (0);
+}
