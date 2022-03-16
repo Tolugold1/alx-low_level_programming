350b7a8df2a9739680a7d3f51bd5e6cfe4bac27e
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-16 11:32:46 -0700

    Task7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -17,12 +17,11 @@ int print_last_digit(int n)
 		int_min = c * -1;
 		int_min = int_min % 10;
 		_putchar(int_min + '0');
-		return (int_min);
-		continue;
-	} else
+	}
+	else
 	{
 		c = c % 10;
 		_putchar(c + '0');
 	}
-	return (c);
+	return (c, int_min);
 }
