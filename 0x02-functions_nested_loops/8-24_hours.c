88464d6e2b7b0d3628c6f7d8bbde0b9047a056d1
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-16 14:05:03 -0700

    Task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,7 +9,18 @@
 void jack_bauer(void)
 {
 	int i;
+	int j;
 
-	do {
-		for (
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
 }
