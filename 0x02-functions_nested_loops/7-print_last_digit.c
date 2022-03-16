1208c07dcf9b1a678a9836dd9869980a0c5703f5
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-16 10:39:58 -0700

    Task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * @int: supplied value.
+ * Return: return the last digit of the int.
+ */
+
+int print_last_digit(int)
+{
+	int = int c;
+	int n;
+
+	n = c % 10;
+	return (n);
+}
