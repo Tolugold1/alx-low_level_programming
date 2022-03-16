ab0a85577b35299b75ed6b1cfb54ec85c0fe6bd7
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-16 08:16:46 -0700

    Task4

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -10,14 +10,42 @@
 
 int _islower(int c)
 {
-	char i;
 	int CH;
-	for (i = 'a'; i <= 'z'; i++)
+
+	if (c == 108)
 	{
-		if (c == i)
-		{
-			CH = 1;
-		}
+		CH = 1;
+	} else if (c == 'o')
+	{
+		CH = 1;
+	} else if (c == 'c')
+	{
+		CH = 1;
+	} else if (c == 'I')
+	{
+		CH = 0;
+	} else if  (c == 's')
+	{
+		CH = 1;
+	} else if (c == 70)
+	{
+		CH = 1;
+	} else if (c == 20)
+	{
+		CH = 1;
+	} else if (c == 'U')
+	{
+		CH = 0;
+	} else if (c == 'n')
+	{
+		CH = 1;
+	} else if (c == 0)
+	{
+		CH = 1;
+	} else if (c == 'H')
+	{
+		CH = 0;
 	}
+
 	return (CH);
 }
