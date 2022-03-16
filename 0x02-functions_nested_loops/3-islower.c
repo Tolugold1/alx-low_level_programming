0a37902c1a7c89214bff7e604b7b6f760336eb22
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-16 07:10:47 -0700

    Task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -11,23 +11,16 @@
 int _islower(int c)
 {
 	int CH;
-	char i;
-	char j;
 
-	do {
-		for (i = 'a'; i <= 'z'; i++)
-		{
-			CH = 1;
-		}
-	} while (c == i);
-	do {
-		for (j = 'A'; j <= 'Z'; j++)
-		{
-			CH = 0;
-		}
-	} while (c == j);
-	do {
+	if (c == 'a' && c <= 'z')
+	{
 		CH = 1;
-	} while (c);
+	} else if (c == 'A' && c <= 'Z')
+	{
+		CH = 0;
+	} else if (c > 0)
+	{
+		CH = 1;
+	}
 	return (CH);
 }
