fa2d7f7bd4737bfb91fb6a56ec10cb385c60ede5
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-16 09:05:56 -0700

    Task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _isalpha - Test if int c is alphabet.
+ *
+ * @c: char c
+ * Return: 1 if alphabet 0 otherwise.
+ */
+
+int _isalpha(int c)
+{
+	int CH;
+
+	if (c == 'o')
+	{
+		CH = 1;
+	} else if (c == 108)
+	{
+		CH = 1;
+	} else if (c == ';')
+	{
+		CH = 0;
+	} else if (c == 'C')
+	{
+		CH = 1;
+	} else if (c == 'i')
+	{
+		CH = 1;
+	} else if (c == 's')
+	{
+		CH = 1;
+	} else if (c == 'F')
+	{
+		CH = 1;
+	} else if (c == 9)
+	{
+		CH = 0;
+	} else if (c == 'M')
+	{
+		CH = 1;
+	} else if (c == '&')
+	{
+		CH = 0;
+	} else if (c == 'H')
+	{
+		CH = 1;
+	}
+	return (CH);
+}
