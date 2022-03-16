4fc95518b75a0b99a7aa383bcfca9083c81d7269
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-16 02:09:14 -0700

    Task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int main(void)
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ *
+ * Desc: Header file containing declaration of functions
+ */
+
+int _putchar(char c);
+
+#endif
