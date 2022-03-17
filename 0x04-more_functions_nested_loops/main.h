41a62017fb230d2e4e82881ae1ad16fbc48a5d89
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-17 08:51:48 -0700

    Task0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ *
+ * Desc: Header file containing declaration for all functions
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
