d06f658f43bd5e6a67e4e1f4f2d2ff4c55b5434b
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-17 10:27:48 -0700

    Task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Multiply
+ * @a: first number
+ * @b: second number
+ * Return: The answer to a x b
+ */
+
+int mul(int a, int b)
+{
+	int i;
+
+	i = a * b;
+	return (i);
+}
