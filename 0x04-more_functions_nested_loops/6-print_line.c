fba8fbd3d773053e7c0131e7fa262131e67e9979
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-17 14:25:26 -0700

    Task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line- function draw line.
+ * @n: number of time to draw _
+ *
+ * Return: absolute 0.
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i == 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('_');
+	}
+	_putchar('\n');
+}
