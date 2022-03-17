ca3be49a1a42b4977c133477449c37726b1be5de
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-17 05:10:37 -0700

    Main.h file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
-/**
- * File: main.h
- *
- * Desc: Header file containing declaration for all functions
- * used in the 0x03.c- Debugging.
- */
 
 
 void positive_or_negative(int i);
