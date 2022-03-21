58c57db81bfef31f5cdb73b713e100a5f729ed17
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-21 02:59:03 -0700

    Task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - pointer function to update a given integer
+ * @n: This the int provider pointer
+ * Return: Absolute zero.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
