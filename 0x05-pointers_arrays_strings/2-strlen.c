e931a01d81194fb8296f3234f7a7ea78794447b4
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-21 05:11:48 -0700

    Task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - function to return the length of a string
+ * @s: The string variable
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; i <= s[i]; i++)
+	{
+		i = i;
+	}
+	return (i);
+}
