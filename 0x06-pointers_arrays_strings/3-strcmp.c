aeb4ab8294ab2a0976195e20a1f7edf6c61aa886
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-23 17:21:47 -0700

    Task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - Function to compare two strings
+ * @s1: first string.
+ * @s2: Second dtring
+ * Return: the results
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, len;
+
+	len = strlen(s1);
+	for (i = 0; i < len; i++)
+	{
+		if (s1 != s2)
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (s1[i] - s2[i]);
+}
