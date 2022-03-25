c39fe934ac97714f14a5fc835e11c26050f603ef
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-25 08:24:19 -0700

    Tsk8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rot13 - Function that encode string using rot13
+ * @s: String to work with
+ * Return: result.
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char alpbetrot13[] =  {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+				'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+				'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+				'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+				's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char ret[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == alpbetrot13[j])
+			{
+				s[i] = ret[j];
+				j = 52;
+			}
+		}
+	}
+	return (s);
+}
