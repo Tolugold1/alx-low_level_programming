57c6f30158f5d5ca58a89900467aca67d9646671
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-28 18:33:35 -0700

    Task2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -18,10 +18,11 @@ char *_strchr(char *s, char c)
 		{
 			return (&s[i]);
 		}
-		else if (s[i] == '\0')
-		{
-			return (&s[i]);
-		}
 	}
-	return (s);
+
+	if (s[i] == c)
+	{
+		return (&s[i]);
+	}
+	return ('\0');
 }
