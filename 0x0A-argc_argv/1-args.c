7e7965a12c3137049b847c5f65d9b9276e7929a6
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-04-01 05:57:57 -0700

    Task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: arguments
+ * Return: absolute 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
