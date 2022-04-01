37f42656009a9156e5f632810c11198fcbc7907f
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-04-01 04:44:38 -0700

    Whatisyourname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * #argv: arguments
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
