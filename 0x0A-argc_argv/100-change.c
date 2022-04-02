854e1ec4c64a2d9aecf2682b96c98edc9f8dbee2
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-04-02 03:59:07 -0700

    update

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -42,37 +42,18 @@ int division(int Q)
 {
 	int ans = 0;
 
-	if ((Q % 10) == 0 && Q < 50)
-	{
-		ans += Q / 10;
-	}
-	else if ((Q % 10) != 0 && Q < 50)
-	{
-		ans +=  (Q / 10) + (Q % 10) - 1;
-	}
-	else if ((Q % 5) == 0 && Q < 20)
-	{
-		ans += Q / 5;
-	}
-	else if ((Q % 5) != 0 && Q < 20)
-	{
-		ans +=  (Q / 5) + (Q % 5) - 1;
-	}
-	else if ((Q % 2) == 0 && Q < 10)
-	{
-		ans += Q / 2;
-	}
-	else if ((Q % 1) == 0 && Q < 2)
-	{
-		ans += Q / 1;
-	}
-	else if ((Q % 25) == 0 && Q >= 25)
-	{
-		ans += Q / 25;
-	}
-	else if ((Q % 25) != 0 && Q >= 25)
-	{
-		ans += (Q / 25) + (Q % 25);
-	}
+	ans += (Q / 25);
+	Q %= 25;
+
+	ans += (Q / 10);
+	Q %= 10;
+
+	ans += (Q / 5);
+	Q %= 5;
+
+	ans += (Q / 2);
+	Q %= 2;
+
+	ans += Q;
 	return (ans);
 }
