0a8d7fa458cd001f59fea7767398d756baab04ca
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-04-11 08:36:12 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - A struct containing a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The name of the dog's owner
+ */
+
 typedef struct dog
 {
 	char *name;
