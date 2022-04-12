48342256cd6f680d77e26762bb947d05b24a788b
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-04-12 04:11:42 -0700

    struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,7 +5,7 @@
  * dog_t *new_dog - function to create a new dog
  * @name: first argument
  * @age: second argument
- * @ownerr: third argument
+ * @owner: third argument
  * Return: result
  */
 
@@ -14,48 +14,51 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 	int len_name, len_owner, i;
 
-	for (len_name = 0; name[len_name] != '\0'; len_name++)
-	{
-		len_name++;
-	}
-
-	for (len_owner = 0; owner[len_owner] != '\0'; len_owner++)
-	{
-		len_owner++;
-	}
+	len_name = _strlen(name);
+	len_owner = _strlen(owner);
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
-
 	new_dog->name = malloc(sizeof(char) * (len_name + 1));
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-
 	for (i = 0; i <= len_name; i++)
 	{
 		new_dog->name[i] = name[i];
 	}
-
 	new_dog->age = age;
-
 	new_dog->owner = malloc(sizeof(char) * (len_owner + 1));
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog);
 		free(new_dog->name);
-		return NULL;
+		return (NULL);
 	}
-
 	for (i = 0; i <= len_owner; i++)
 	{
-		new_dog->owner [i] = owner[i];
+		new_dog->owner[i] = owner[i];
 	}
-
 	return (new_dog);
+}
+
+/**
+ * _strlen - function to calculate the length of a string
+ * @c: string.
+ * Return: result.
+ */
+
+int _strlen(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+	}
+	return (i);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,5 +17,6 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
+int _strlen(char *c);
 dog_t *new_dog(char *name, float age, char *owner);
 #endif
