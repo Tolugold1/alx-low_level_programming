628b0945b0ecd83168ab337d32aa48971c635cd7
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-04-13 11:11:02 -0700

    func pointer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - funtion iterator
+ * @array:  pointer to an array
+ * @size: size of the array
+ * @action: function pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long int i;
+
+	if (action == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
