27aa6a83c6d62ef5d30880427bcf2f9eecce4c92
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-04-14 05:37:23 -0700

    calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -3,6 +3,8 @@
 
 /**
  * struct op - structure type
+ * @op: The oprator
+ * @T: The function pointer
  * Return: value.
  */
 
@@ -16,7 +18,7 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char *s))(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
 #endif
 
