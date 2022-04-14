4580f001a8eaa0a0d03fbab515fa2ce2bfa7237d
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-04-13 19:40:57 -0700

    function pointer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,13 +12,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (cmp == NULL || array == NULL || size <= 0)
 	{
 		return (-1);
-	}
-	if (cmp == NULL)
-	{
-		return (0);
 	}
 
 	for (i = 0; i < size; i++)
