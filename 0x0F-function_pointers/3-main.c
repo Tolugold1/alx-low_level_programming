e10405aa0a121d0f961a3c336d8a26de120cadff
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-04-14 05:23:23 -0700

    func ptr

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,6 +17,12 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && (atoi(argv[3]) == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
