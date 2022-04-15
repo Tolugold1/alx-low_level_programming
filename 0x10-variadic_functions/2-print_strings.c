c6dc7d2d039654ff49f6b9b3363d7cf2a43387c6
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-04-15 03:07:22 -0700

    v.f

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			string = va_arg(p_string, char *);
 			if (string == NULL)
 			{
-				string = "nil";
+				string = "(nil)";
 			}
 			printf("%s", string);
 		}
@@ -39,7 +39,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			string = va_arg(p_string, char *);
 			if (string == NULL)
 			{
-				string = "nil";
+				string = "(nil)";
 			}
 			printf("%s%s", string, separator);
 		}
