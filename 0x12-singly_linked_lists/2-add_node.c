62ca6717c130419efcd0ce7be691b33f93d2ac8a
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-04-21 09:04:50 -0700

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -27,3 +27,19 @@ list_t *add_node(list_t **head, const char *str)
 	*head = new;
 	return (*head);
 }
+
+/**
+ * _strlen - function to calculate the length of a const char string
+ * @s: string to work with
+ * Return: length of string.
+ */
+
+int _strlen(const char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+#include <string.h>
 #include <stddef.h>
 /**
  * struct list_s - singly linked list
@@ -22,21 +24,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
-/**
- * _strlen - function to calculate the length of a const char string
- * @s: string to work with
- * Return: length of string.
- */
-
-int _strlen(const char *s)
-{
-	unsigned int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-	}
-	return (i);
-}
+int _strlen(const char *s);
 
 #endif
