100072e07b62de599aee40913a5d509631307a8e
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-04-22 01:49:36 -0700

    free singly list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - function to free singly list
+ * @head: the node to be freed
+ * Return: void.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+	temp = head;
+	while (temp != NULL)
+	{
+		list_t *fr;
+		fr = temp;
+		temp = temp->next;
+		free(fr->str);
+		free(fr);
+	}
+	head = NULL;
+}
