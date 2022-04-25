000173ec05331b22c05d20b5e316aee4dc2534dd
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-04-25 01:48:22 -0700

    return number of element

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,7 +10,7 @@
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *temp;
-	int i;
+	size_t i;
 
 	temp = h;
 	for (i = 0; temp; i++)
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - function that returns the number
+ * of elements in a linked listint_t list.
+ * @h: headnode
+ *
+ * Return: number of elements.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *temp;
+	size_t i;
+
+	temp = h;
+	for (i = 0; temp; i++)
+	{
+		temp = temp->next;
+	}
+	return (i);
+}
