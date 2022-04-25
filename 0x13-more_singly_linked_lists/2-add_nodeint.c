c0cd261e9cd42c4172894d9d3c965f5b4a33e71b
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-04-25 11:03:08 -0700

    add node to to head

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,6 +12,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newNode;
 
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
+
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
 	{
