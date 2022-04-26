c971d0627b19ba884617d84857a76d676c87ba29
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-04-26 06:28:55 -0700

    sum of data

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_listint - function that returns the sum of all the data 
+ * @head: head pointer
+ * Return: result.
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
