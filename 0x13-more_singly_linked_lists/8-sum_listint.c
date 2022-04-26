cda9a2800ed29692d8c1372e223c4c3d4d33a7ab
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-04-26 06:43:42 -0700

    sum of data

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_listint - function that returns the sum of all the data 
+ * sum_listint - function that returns the sum of all the data
  * @head: head pointer
  * Return: result.
  */
