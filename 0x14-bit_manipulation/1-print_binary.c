3a8ebe76fb5cca9b26ba4cd1e461e9c3a1e9574d
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-04-28 09:19:09 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - Print the binary representation of a number
+ * @n: The number
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int i;
+	int start_printing = 0;
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		for (i = 1UL << 63; i > 0; i >>= 1)
+		{
+			if (n & i)
+			{
+				_putchar('1');
+				start_printing = 1;
+			}
+			else if (start_printing)
+				_putchar('0');
+		}
+	}
+}
