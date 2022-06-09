69b60b111c8f1cef803e7a46cc4ad5bb8760c419
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-06-09 06:34:15 -0700

    update

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,12 +15,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (!head)
 	{
-		return NULL;
+		return (NULL);
 	}
 	newNode = malloc(sizeof(dlistint_t));
 	if (newNode == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	newNode->n = n;
 	newNode->prev = NULL;
