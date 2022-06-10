c99722574914b71867b2f90aaf6e40c24f428142
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-06-10 03:08:23 -0700

    insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,99 +1,53 @@
 #include "lists.h"
-
 /**
  * insert_dnodeint_at_index - function to insert node at
  * a particular index
  * @h: head pointer
  * @idx: index location
  * @n: new node integer
- * Return: address of the new node, or NULL if it failed
+ * Return: new node address
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int list_len;
-	dlistint_t *new;
-	dlistint_t *temp;
-	dlistint_t *temp2;
+	unsigned int x = 0;
+	dlistint_t *temp = *h;
+	dlistint_t *node = malloc(sizeof(dlistint_t));
 
-	temp = *h;
-	list_len = dlistint_length(temp);
+	if (!node)
+		return (NULL);
 	if (!h)
-	{
 		return (NULL);
-	}
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
-	{
-		return (NULL);
-	}
-	new->n = n;
-
-	if (*h == NULL)
-	{
-		new->prev = NULL;
-		new->next = NULL;
-		(*h) = new;
-		return (new);
-	}
+	node->n = n;
 	if (idx == 0)
 	{
-		temp->prev = new;
-		new->prev = NULL;
-		new->next = temp;
-		return (new);
-	}
-	else if (idx == 1)
-	{
-		(*h)->next = new;
-		new->prev = (*h);
-		new->next = (*h)->next->next;
-		return (new);
-	}
-	else if (idx == list_len)
-	{
-		while (temp)
+		if (!*h)
 		{
-			temp = temp->next;
-		}
-		temp->next = new;
-		new->prev = temp;
-		new->next = NULL;
-		return (new);
-	}
-	else
+			node->prev = NULL;
+			node->next = NULL; }
+		else
+		{	node->prev = NULL;
+			node->next = *h;
+			(*h)->prev = node; }
+		*h = node;
+		return (node); }
+	while (temp)
 	{
-		while (idx > 1)
+		if (idx == x + 1)
 		{
-			temp = temp->next;
-			idx--;
-		}
-		temp2 = temp->next;
-		temp->next = new;
-		new->prev = temp;
-		new->next = temp2;
-		temp2->prev = new;
-		return (new);
-	}
-	free(new);
-	return (NULL);
-}
-
-/**
- * dlistint_length - function that returns the number
- * of elements in a linked dlistint_t list.
- * @h: head pointer
- * Return: the number of elements in a linked list
- */
-
-size_t dlistint_length(dlistint_t *h)
-{
-	size_t i = 0;
-
-	while (h)
-	{
-		h = h->next;
-		i++;
-	}
-	return (i);
-}
+			if (temp->next == NULL)
+			{
+				node->next = NULL;
+				node->prev = temp;
+				temp->next = node; }
+			else
+			{
+				node->next = temp->next;
+				node->prev = temp;
+				temp->next = node;
+				node->next->prev = node; }
+			return (node); }
+		x++;
+		temp = temp->next; }
+	free(node);
+	return (NULL); }
