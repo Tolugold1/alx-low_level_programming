05735e5d63b32248a8c691045ca245994619dc57
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-07-01 01:44:13 -0700

    update

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,9 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *temp = NULL;
 
-	if (ht == NULL)
-		return (NULL);
-	if (key == NULL)
+	if (ht == NULL || key == NULL)
 		return (NULL);
 
 	index = key_index((unsigned char *)key, ht->size);
