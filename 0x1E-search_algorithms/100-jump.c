ab0938e77da1cebcf9b19f4fc23d8331c0b7fea3
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-10-04 01:40:45 -0700

    update

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -24,7 +24,7 @@ int jump_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", y, array[y]);
 		y += z;
-	       	if (y >= size)
+		if (y >= size)
 		{
 			break;
 		}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,5 +7,8 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void printArray(int *array, size_t low, size_t high);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 
 #endif
